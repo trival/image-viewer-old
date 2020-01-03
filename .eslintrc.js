f3b49ef261f8dc3d72c65e89ecc2268b990587b9
Thomas Gorny <thomas.gorny@pm.me>
2020-01-03 16:35:13 +0100

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,37 @@
 module.exports = {
-  root: true,
+	root: true,
 
-  parserOptions: {
-    parser: 'babel-eslint',
-    sourceType: 'module'
-  },
+	parserOptions: {
+		parser: 'babel-eslint',
+		sourceType: 'module',
+	},
 
-  env: {
-    browser: true
-  },
+	env: {
+		browser: true,
+	},
 
-  // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-  // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-  extends: [
-    'plugin:vue/essential',
-    '@vue/prettier'
-  ],
+	// https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+	// consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+	extends: ['plugin:vue/essential', '@vue/prettier'],
 
-  // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
+	// required to lint *.vue files
+	plugins: ['prettier', 'vue'],
 
-  globals: {
-    'ga': true, // Google Analytics
-    'cordova': true,
-    '__statics': true,
-    'process': true,
-    'Capacitor': true
-  },
+	globals: {
+		ga: true, // Google Analytics
+		cordova: true,
+		__statics: true,
+		process: true,
+		Capacitor: true,
+	},
 
-  // add your custom rules here
-  rules: {
-    'prefer-promise-reject-errors': 'off',
+	// add your custom rules here
+	rules: {
+		'prefer-promise-reject-errors': 'off',
 
-    // allow console.log during development only
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    // allow debugger during development only
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
+		// allow console.log during development only
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		// allow debugger during development only
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+	},
 }
